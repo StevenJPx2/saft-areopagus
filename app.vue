8b8fe6359fc3a7f1d08652cdcd498faa105f97a4
Steven John <33600496+StevenJPx2@users.noreply.github.com>
2021-12-24 19:20:16 +0400

    style(ios): added styling

--- app.vue
+++ app.vue
@@ -108,6 +108,18 @@ main {
     user-select: none;
   }
 
+   #permission {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    z-index: 1000;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: grid;
+    place-content: center;
+  }
+
   #hero {
     display: grid;
     place-items: center;
