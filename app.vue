df6e1f2f7d8b4cef341c092c47fc5de4074130a4
Steven John <stevenjpx@gmail.com>
2021-12-26 18:39:34 +0400

    fix(app): reverting the device orientation ios fix

--- app.vue
+++ app.vue
@@ -12,6 +12,7 @@ tryOnMounted(() => {
     typeof DeviceMotionEvent !== "undefined" &&
     typeof DeviceMotionEvent.requestPermission === "function"
   )
+    isDeviceOrientation.value = true;
     useDeviceOrientation();
 });
 
@@ -27,8 +28,8 @@ const { tilt, roll } = useParallax(mainRef);
 const parallax = (mag: number) =>
   computed(
     (): CSSProperties => ({
-      transform: `translate(${tilt.value * mag * (isMobile.value ? 1 : 3)}px, ${
-        roll.value * -mag * (isMobile.value ? 1 : 3)
+      transform: `translate(${tilt.value * mag * (isMobile.value ? 1 : 4)}px, ${
+        roll.value * -mag * (isMobile.value ? 1 : 4)
       }px)`,
     })
   ).value;
@@ -130,6 +131,18 @@ main {
     user-select: none;
   }
 
+   #permission {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    z-index: 1000;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: grid;
+    place-content: center;
+  }
+
   #hero {
     display: grid;
     place-items: center;
@@ -146,7 +159,7 @@ main {
 
     #mountain {
       width: 90vw;
-      margin-bottom: -9vw;
+      margin-bottom: -8vw;
     }
 
     #areopagus-2022-typeform {
