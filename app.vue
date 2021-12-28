40ba3a53050ccbb72d564bc1250af5d4bd1071ac
Steven John <stevenjpx@gmail.com>
2021-12-28 10:24:34 +0400

    test(debug): changed readability

--- app.vue
+++ app.vue
@@ -115,7 +115,7 @@ watchAtMost(
   </Html>
 
   <main ref="mainRef">
-    <div class="debug"> {{ tilt }} {{ roll }} {{ initTilt }} {{ initRoll }}</div>
+    <div class="debug"> {{ tilt.toFixed(2) }} {{ roll.toFixed(2) }} {{ initTilt.toFixed(2) }} {{ initRoll.toFixed(2) }}</div>
     <div v-show="isDeviceOrientation" id="permission">
       <button @click="useDeviceOrientation()">Use Device Orientation</button>
     </div>
